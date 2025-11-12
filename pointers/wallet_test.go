91e4ac7becd104a912c1609c3ca3c363f231a645
Boris <boris@xx1.nl>
2025-11-12 15:58:53 +0100

    Pointers

---
+++ pointers/wallet_test.go
@@ -0,0 +1,58 @@
+package pointers
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	assertError := func(t testing.TB, err error, want error) {
+		t.Helper()
+		if err == nil {
+			t.Fatal("no error provided")
+		}
+
+		if !errors.Is(err, want) {
+			t.Error("error is not of correct format")
+		}
+	}
+
+	assertNoError := func(t testing.TB, err error) {
+		if err != nil {
+			t.Fatal("got an error but didn't expected one")
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
+	})
+}
